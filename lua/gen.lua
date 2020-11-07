308ed339875fed02231db35fa4a053b3e21f8744
abehmiel <abehmiel@gmail.com>
2020-11-06 23:43:38 -0500

    refactor for readability, max lives, firebar objects

--- lua/gen.lua
+++ lua/gen.lua
@@ -3,107 +3,138 @@
 -- Initialize story file
 file = io.open('../txt/story.txt','w')
 
-objLookup = {[0x00] = "Green Koopa Troopa",
-			 [0x01] = "Red Koopa Troopa",
-			 [0x02] = "Buzzy beetle",
-			 [0x03] = "Red Koopa Troopa",
-			 [0x04] = "Green Koopa Troopa",
-			 [0x05] = "Hammer brother",
-			 [0x06] = "Goomba",
-			 [0x07] = "Blooper",
-			 [0x08] = "Bullet Bill",
-			 [0x09] = "Green Koopa Paratroopa",
-			 [0x0A] = "Grey CheepCheep",
-			 [0x0B] = "Red CheepCheep",
-			 [0x0C] = "Pobodoo",
-			 [0x0D] = "Piranha Plant",
-			 [0x0E] = "Green Jumping Paratroopa",
-			 [0x10] = "Bowser's flame",
-			 [0x11] = "Lakitu",
-			 [0x12] = "Spiny",
-			 [0x14] = "Flying CheepCheep",
-			 [0x15] = "Bowser's Flame",
-			 [0x16] = "fireworks",
-			 [0x17] = "Bullet Bill",
-			 [0x20] = "firebar",
-			 [0x21] = "long firebar",
-			 [0x24] = "static platform",
-			 [0x25] = "static platform",
-			 [0x26] = "vertical moving platform",
-			 [0x27] = "elevator platform",
-			 [0x28] = "horizontal moving platform",
-			 [0x29] = "buckling platform",
-			 [0x2A] = "horizontal moving platform",
-			 [0x2B] = "elevator platform",
-			 [0x2C] = "elevator platform",
-			 [0x2D] = "Bowser",
-			 [0x2F] = "vine ",
-			 [0x30] = "flagpole",
-			 [0x32] = "jump spring",
-			 [0x33] = "Bullet Bill Cannon",
-			 [0x34] = "Warp Zone",
-			 [0x35] = "Toad",
-			 [0x37] = "Goombas in a row",
-			 [0x38] = "Goombas in a row",
-			 [0x3A] = "skewed Goomba",
-			 [0x3B] = "Koopa Troopas in a row",
-			 [0x3C] = "Koopa Troopas in a row"
+objLookup = {
+    [0x00] = "Green Koopa Troopa",
+	[0x01] = "Red Koopa Troopa",
+	[0x02] = "Buzzy beetle",
+    [0x03] = "Red Koopa Troopa",
+	[0x04] = "Green Koopa Troopa",
+	[0x05] = "Hammer brother",
+	[0x06] = "Goomba",
+	[0x07] = "Blooper",
+	[0x08] = "Bullet Bill",
+	[0x09] = "Green Koopa Paratroopa",
+	[0x0A] = "Grey CheepCheep",
+	[0x0B] = "Red CheepCheep",
+	[0x0C] = "Podoboo",
+	[0x0D] = "Piranha Plant",
+	[0x0E] = "Green Jumping Paratroopa",
+	[0x10] = "Bowser's flame",
+	[0x11] = "Lakitu",
+	[0x12] = "Spiny",
+	[0x14] = "Flying CheepCheep",
+	[0x15] = "Bowser's Flame",
+	[0x16] = "fireworks",
+	[0x17] = "Bullet Bill",
+	[0x1B] = "clockwise firebar",
+	[0x1C] = "quick clockwise firebar",
+	[0x1D] = "counter-clockwise firebar",
+	[0x1E] = "quick counter-clockwise firebar",
+	[0x1F] = "long firebar",
+	[0x20] = "firebar",
+	[0x21] = "long firebar",
+	[0x22] = "firebar",
+	[0x24] = "static platform",
+	[0x25] = "static platform",
+	[0x26] = "vertical moving platform",
+	[0x27] = "elevator platform",
+	[0x28] = "horizontal moving platform",
+	[0x29] = "buckling platform",
+	[0x2A] = "horizontal moving platform",
+	[0x2B] = "elevator platform",
+	[0x2C] = "elevator platform",
+	[0x2D] = "Bowser",
+	[0x2F] = "vine",
+	[0x30] = "flagpole",
+	[0x32] = "jump spring",
+	[0x33] = "Bullet Bill Cannon",
+	[0x34] = "Warp Zone",
+	[0x35] = "Toad",
+	[0x37] = "Goombas in a row",
+	[0x38] = "Goombas in a row",
+	[0x3A] = "skewed Goomba",
+	[0x3B] = "Koopa Troopas in a row",
+	[0x3C] = "Koopa Troopas in a row"
 }
 
-objArticleLookup = {[0x00] = "a ",
-			 [0x01] = "a ",
-			 [0x02] = "a ",
-			 [0x03] = "a ",
-			 [0x04] = "a ",
-			 [0x05] = "a ",
-			 [0x06] = "a ",
-			 [0x07] = "a ",
-			 [0x08] = "a ",
-			 [0x09] = "a ",
-			 [0x0A] = "a ",
-			 [0x0B] = "a ",
-			 [0x0C] = "a ",
-			 [0x0D] = "a ",
-			 [0x0E] = "a ",
-			 [0x10] = "a ",
-			 [0x11] = "a ",
-			 [0x12] = "a ",
-			 [0x14] = "a ",
-			 [0x15] = "a ",
-			 [0x16] = "",
-			 [0x17] = "a ",
-			 [0x20] = "a ",
-			 [0x21] = "a ",
-			 [0x24] = "a ",
-			 [0x25] = "a ",
-			 [0x26] = "a ",
-			 [0x27] = "an ",
-			 [0x28] = "a ",
-			 [0x29] = "a ",
-			 [0x2A] = "a ",
-			 [0x2B] = "an ",
-			 [0x2C] = "an ",
-			 [0x2D] = "",
-			 [0x2F] = "a ",
-			 [0x30] = "a ",
-			 [0x32] = "a ",
-			 [0x33] = "a ",
-			 [0x34] = "a ",
-			 [0x35] = "",
-			 [0x37] = "two ",
-			 [0x38] = "three ",
-			 [0x3A] = "a ",
-			 [0x3B] = "two ",
-			 [0x3C] = "three "
+objArticleLookup = {
+    [0x00] = "a ",
+   	[0x01] = "a ",
+	[0x02] = "a ",
+	[0x03] = "a ",
+	[0x04] = "a ",
+	[0x05] = "a ",
+	[0x06] = "a ",
+	[0x07] = "a ",
+	[0x08] = "a ",
+	[0x09] = "a ",
+	[0x0A] = "a ",
+	[0x0B] = "a ",
+	[0x0C] = "a ",
+	[0x0D] = "a ",
+	[0x0E] = "a ",
+	[0x10] = "a ",
+	[0x11] = "a ",
+	[0x12] = "a ",
+	[0x14] = "a ",
+	[0x15] = "a ",
+	[0x16] = "",
+	[0x17] = "a ",
+	[0x1B] = "a ",
+	[0x1C] = "a ",
+	[0x1D] = "a ",
+	[0x1E] = "a ",
+	[0x1F] = "a ",
+	[0x17] = "a ",
+	[0x20] = "a ",
+	[0x21] = "a ",
+	[0x24] = "a ",
+	[0x25] = "a ",
+	[0x26] = "a ",
+	[0x27] = "an ",
+	[0x28] = "a ",
+	[0x29] = "a ",
+	[0x2A] = "a ",
+	[0x2B] = "an ",
+	[0x2C] = "an ",
+	[0x2D] = "",
+	[0x2F] = "a ",
+	[0x30] = "a ",
+	[0x32] = "a ",
+	[0x33] = "a ",
+	[0x34] = "a ",
+	[0x35] = "",
+	[0x37] = "two ",
+	[0x38] = "three ",
+	[0x3A] = "a ",
+	[0x3B] = "two ",
+	[0x3C] = "three "
 }
+
+--Initialize stuff
+runningScore = 0
+lastRunningScore = 0
+powerUpState = 0
+world = nil
+level = nil
+oldLives = 3
+floatState = 0x00
+oldDrawn = {0, 0, 0, 0, 0}
+oldviews = {0, 0, 0, 0, 0}
+oldObjects = {0, 0, 0, 0, 0}
+oldObjStates = {0, 0, 0, 0, 0}
+oldBrickState1 = 0
+oldBrickState2 = 0
+brickon = 0
+lifeAdjustFlag = false
 
 function getScore()
-    return memory.readbyte(0x07DD)*1000000 +
-           memory.readbyte(0x07DE)*100000 +
-           memory.readbyte(0x07DF)*10000 +
-           memory.readbyte(0x07E0)*1000 +
-           memory.readbyte(0x07E1)*100 +
-           memory.readbyte(0x07E2)*10           
+    return memory.readbyte(0x07DD) * 1000000 +
+           memory.readbyte(0x07DE) * 100000 +
+           memory.readbyte(0x07DF) * 10000 +
+           memory.readbyte(0x07E0) * 1000 +
+           memory.readbyte(0x07E1) * 100 +
+           memory.readbyte(0x07E2) * 10           
 end
 
 function getObjects()
@@ -202,11 +233,17 @@ function levelPhrase(lv, wo, text)
     local phrase = ""
     local fort = fortPhrase(lv, wo)
     if fort ~= nil then 
-        phrase = phrase..fort.."\n\nChapter "..wo.."-"..lv.." \n \n"
+        phrase = phrase..fort.."\n\nChapter "..wo.."-"..lv.." \n\n"
     else
-        phrase = "\n\nChapter "..wo.."-"..lv.." \n \n"
+        phrase = "\n\nChapter "..wo.."-"..lv.." \n\n"
     end
     text = safeAppend(text, phrase)
+
+    -- hack to monkey around with how many lives we have
+    if (wo == 1) and (lv == 1) and (lifeAdjustFlag == false) then
+        memory.writebyte(0x075A, 100)
+        lifeAdjustFlag = true
+    end
     return text
 end
 
@@ -308,9 +345,9 @@ function floatPhrase(floatState, spriteState, text)
 end
 
 function skidPhrase(text)
---    local phrase = "Mario abruptly changes his direction and starts skidding. "
---    text = safeAppend(text, phrase)
---    return text
+    local phrase = "Mario abruptly changes his direction and starts skidding. "
+    text = safeAppend(text, phrase)
+    return text
 end
 
 function spritePhrase(spriteState, oldSpriteState, text)
@@ -320,7 +357,7 @@ function spritePhrase(spriteState, oldSpriteState, text)
     elseif spriteState == 0x0B then 
         phrase = phrase.."Mario shrugs and slinks away in defeat. "
     elseif spriteState == 0x06 and lives ~= 1 then
-        phrase = phrase.."Mario stands up, brushes off his overalls, and gets ready for another go. "
+        phrase = phrase.."\n\nMario stands up, brushes off his overalls, and gets ready for another go. "
     elseif spriteState == 0x01 then
         phrase = phrase.."Mario climbs a vine. "
     elseif spriteState == 0x07 then
@@ -419,9 +456,11 @@ function fortPhrase(lv, wo)
     elseif (wo ~= 1 and lv == 1) then
         if world < 8 then
             phrase = phrase..'Toad says, "Thank you Mario! But our princess is in another castle!" '
-            phrase = phrase.."Mario leaves the castle. "
+            phrase = phrase.."Mario leaves the castle, ready to continue his search for the princess. "
         else
-            phrase = phrase.."It's Princess Peach! Mario has finally saved her from Bowser. She says, \"Thank you Mario! Your quest is over. Push button B to select a world.\" Mario leaves the castle. "
+            phrase = phrase.."It's Princess Peach! Mario has finally saved her from Bowser. "
+            phrase = phrase.."She says, \"Thank you Mario! Your quest is over. Push button B to select a world.\""
+            phrase = phrase.."Mario triumphantly leaves the castle. "
         end
     end
     text = safeAppend(text, phrase)
@@ -518,25 +557,10 @@ function frameRuleUpdate()
 
     --debug
     --emu.message(objStates[1]..objStates[2]..objStates[3]..objStates[4]..objStates[5])
-    --emu.message(memory.readbyte(0x0709))
+    --emu.message(memory.readbyte(0x0702))
 
 end
 
---Initialize stuff
-runningScore = 0
-lastRunningScore = 0
-powerUpState = 0
-world = nil
-level = nil
-oldLives = 3
-floatState = 0x00
-oldDrawn = {0, 0, 0, 0, 0}
-oldviews = {0, 0, 0, 0, 0}
-oldObjects = {0, 0, 0, 0, 0}
-oldObjStates = {0, 0, 0, 0, 0}
-oldBrickState1 = 0
-oldBrickState2 = 0
-brickon = 0
 
 --Infinite loop to take control over frame advancing.
 while true do
@@ -640,7 +664,7 @@ while true do
 
     --Check skidding
     if walkAnimation ~= oldWalkAnimation and oldWalkAnimation ~= nil and 
-       (walkAnimation == 0x30 or 0xA0) then
+       (walkAnimation == 0xA0) then
         text = skidPhrase(text)
     end
     oldWalkAnimation = walkAnimation
